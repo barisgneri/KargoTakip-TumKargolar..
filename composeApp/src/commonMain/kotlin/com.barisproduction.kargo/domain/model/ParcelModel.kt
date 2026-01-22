f59aef8c88de94ea0e2f77afb174ecb8634c10b6
barisgneri <barisgnr.tr@gmail.com>
2026-01-22 20:01:34 +0300

    Update ParcelModel.kt

--- composeApp/src/commonMain/kotlin/com.barisproduction.kargo/domain/model/ParcelModel.kt
+++ composeApp/src/commonMain/kotlin/com.barisproduction.kargo/domain/model/ParcelModel.kt
@@ -5,12 +5,12 @@ enum class ParcelModel(
     val trackingUrl: String,
     val logo: String
 ) {
-    YURTICI("Yurtiçi Kargo", "https://gonderitakip.yurticikargo.com/t?t=", "logo_yurtici"),
-    ARAS("Aras Kargo", "http://kargotakip.araskargo.com.tr/mainpage.aspx?code=", "logo_aras"),
-    MNG("MNG Kargo", "http://service.mngkargo.com.tr/iactive/popup/kargotakip.asp?k=", "logo_mng"),
+    YURTICI("Yurtiçi Kargo", "https://www.yurticikargo.com/tr/online-servisler/gonderi-sorgula?code=", "logo_yurtici"),
+    ARAS("Aras Kargo", "https://ebranch.araskargo.com.tr/track/", "logo_aras"),
+    MNG("DHL Kargo (MNG)", "http://service.mngkargo.com.tr/iactive/popup/kargotakip.asp?k=", "logo_mng"),
     SURAT("Sürat Kargo", "http://kargoweb.suratkargo.com.tr/kargotakip.aspx?p=", "logo_surat"),
     PTT("PTT Kargo", "https://gonderitakip.ptt.gov.tr/Track/Verify?q=", "logo_ptt"),
-    UPS("UPS Kargo", "https://www.ups.com.tr/WaybillQueryResult.aspx?Waybill=", "logo_ups"),
+    UPS("HepsiJet", "https://www.ups.com.tr/WaybillQueryResult.aspx?Waybill=", "logo_ups"),
     OTHER("Diğer", "", "logo_default");
 
     companion object {
